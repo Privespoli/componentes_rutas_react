2adda9599e592802161c89deec97e0f286a366fb
Priscila Nunes <priscilavespoli92@gmail.com>
2026-04-24 10:15:50 +0200

    trabajando

---
+++ src/components/forms/Form.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <input type="submit"  value="submit" />
+        </form>
+    </>
+  );
+  }
+
+export default Form;
